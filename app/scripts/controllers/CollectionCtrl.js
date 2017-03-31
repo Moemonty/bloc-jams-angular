43909e8a1f2900d5743766705e2d3e6135b4d879
Ashley <ashleyjnelms@gmail.com>
2017-03-31 16:18:30 -0400

    CollectionCtrl.js updated

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -4,12 +4,8 @@
      for (var i=0; i < 12; i++) {
          this.albums.push(angular.copy(albumPicasso));
      }
-<<<<<<< HEAD
-    }
-=======
      }
->>>>>>> checkpoint-5-controllers
- 
+     
      angular
          .module('blocJams')
          .controller('CollectionCtrl', CollectionCtrl);
--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -4,9 +4,12 @@
      for (var i=0; i < 12; i++) {
          this.albums.push(angular.copy(albumPicasso));
      }
-    }
- 
+     }
+     
      angular
          .module('blocJams')
          .controller('CollectionCtrl', CollectionCtrl);
  })();
+
+
+//update//
