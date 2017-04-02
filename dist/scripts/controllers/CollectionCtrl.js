2ef66348f447b079ad3f150803dd59c963e4deb6
Ashley <ashleyjnelms@gmail.com>
2017-04-01 21:22:26 -0400

    Assignment 6 completed

--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -1,15 +1,10 @@
- (function() {
-     function CollectionCtrl() {
-         this.albums = [];
-     for (var i=0; i < 12; i++) {
-         this.albums.push(angular.copy(albumPicasso));
-     }
-     }
-     
-     angular
-         .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
- })();
-
-
-//update//
+(function() {
+    function CollectionCtrl(Fixtures) {
+        this.albums = Fixtures.getCollection(12);
+        
+    }
+    
+    angular 
+        .module('blocJams')
+        .controller('CollectionCtrl', CollectionCtrl);
+})();
